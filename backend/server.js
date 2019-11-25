be68a4a058a697e3495019523760a7c3365d1f81
Quang <quangndn@necsv-hcm.com>
2019-11-25 17:40:23 +0700

    update source

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var app = express()
-var db = require("./database.js")
+//var db = require("./database.js")
+var fetch = require('node-fetch')
+var async = require("async");
 var md5 = require("md5")
 
 var bodyParser = require("body-parser");
@@ -11,23 +13,57 @@ var HTTP_PORT = 8000
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-app.get("/api/articles", (req, res, next) => {
-    var sql = "select * from articles"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
+//get data from pizza
+IfoundData = (newSource) => {
+    return fetch(`http://www.mocky.io/v2/${newSource}`, {
+        method: 'GET',
+        mode: "no-cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" }
+        }).then(res => {
+            return res.json();
+        }).catch(res => {
+            console.log("Exception : ", res);
         })
-      });
+}
+insertNewsDB = () => {
+
+}
+app.post("/api/articles", (req, res, next) => {
+    let newSource = "5dc01bc8310000b288be3e37";
+    if (req.body.newSource) {
+        newSource = req.body.newSource;
+    }
+    let pizzaSource = async() => {
+        let data = await IfoundData(newSource);
+        return data;
+    }
+    pizzaSource().then(data => {
+        res.json({
+            "message": "success",
+            "data": data
+        })
+    })
+ 
 });
+// app.get("/api/articles", (req, res, next) => {
+//     var sql = "select * from articles"
+//     var params = []
+//     db.all(sql, params, (err, rows) => {
+//         if (err) {
+//           res.status(400).json({"error":err.message});
+//           return;
+//         }
+//         res.json({
+//             "message":"success",
+//             "data":rows
+//         })
+//       });
+// });
 
 
 app.get("/api/article/:id", (req, res, next) => {
@@ -35,52 +71,52 @@ app.get("/api/article/:id", (req, res, next) => {
     var params = [req.params.id]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
-            "message":"success",
-            "data":row
+            "message": "success",
+            "data": row
         })
-      });
+    });
 });
 
 
 app.post("/api/article/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
+    var errors = []
+    if (!req.body.password) {
         errors.push("No password specified");
     }
-    if (!req.body.email){
+    if (!req.body.email) {
         errors.push("No email specified");
     }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
         return;
     }
     var data = {
-        author: req.body.author, 
-        title: req.body.title, 
+        author: req.body.author,
+        title: req.body.title,
         description: req.body.description,
-        url: req.body.url, 
-        urlToImage: req.body.urlToImage, 
-        publishedAt: req.body.publishedAt, 
+        url: req.body.url,
+        urlToImage: req.body.urlToImage,
+        publishedAt: req.body.publishedAt,
         content: req.body.content,
         sourceID: req.body.sourceID,
         sourceName: req.body.sourceName
     }
-    var sql = 'INSERT INTO articles (author, title, description, url, urlToImage, publishedAt, content, sourceID, sourceName)'
-            += 'VALUES (?,?,?,?,?,?,?,?,?)'
-    var params =[data.author, data.title, data.description, data.url, data.urlToImage, data.publishedAt, data.content, data.sourceID, data.sourceName]
+    var sql = 'INSERT INTO articles (author, title, description, url, urlToImage, publishedAt, content, sourceID, sourceName) \n'
+    'VALUES (?,?,?,?,?,?,?,?,?)'
+    var params = [data.author, data.title, data.description, data.url, data.urlToImage, data.publishedAt, data.content, data.sourceID, data.sourceName]
     db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
+        if (err) {
+            res.status(400).json({ "error": err.message })
             return;
         }
         res.json({
             "message": "success",
             "data": data,
-            "id" : this.lastID
+            "id": this.lastID
         })
     });
 })
@@ -91,7 +127,7 @@ app.patch("/api/article/:id", (req, res, next) => {
     var data = {
         name: req.body.name,
         email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : undefined
+        password: req.body.password ? md5(req.body.password) : undefined
     }
     db.run(
         `UPDATE article set 
@@ -101,15 +137,15 @@ app.patch("/api/article/:id", (req, res, next) => {
            WHERE id = ?`,
         [data.name, data.email, data.password, req.params.id],
         (err, result) => {
-            if (err){
-                res.status(400).json({"error": res.message})
+            if (err) {
+                res.status(400).json({ "error": res.message })
                 return;
             }
             res.json({
                 message: "success",
                 data: data
             })
-    });
+        });
 })
 
 
@@ -118,17 +154,17 @@ app.delete("/api/article/:id", (req, res, next) => {
         'DELETE FROM article WHERE id = ?',
         req.params.id,
         function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
+            if (err) {
+                res.status(400).json({ "error": res.message })
                 return;
             }
-            res.json({"message":"deleted", rows: this.changes})
-    });
+            res.json({ "message": "deleted", rows: this.changes })
+        });
 })
 
 
 // Root path
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    res.json({ "message": "Ok" })
 });
 
